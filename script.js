21c42309cfec0c7c61e706f2c44442daf445a9ab
monjove99 <monjove99@gmail.com>
2023-12-11 09:33:30 +0800

    Added a function that resets the container background

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function populateContainer(size) {
   let container = document.querySelector(".container");
-  let squares = document.querySelectorAll("div");
+  let squares = container.querySelectorAll("div");
   squares.forEach((div) => div.remove());
-  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
   let amount = size * size;
   for (let i = 0; i < amount; i++) {
     let square = document.createElement("div");
     square.classList.add("cell");
-    container.style.backgroundColor = "#e0e0e0";
     square.addEventListener("mouseover", colorSquare);
     container.insertAdjacentElement("beforeend", square);
   }
@@ -17,9 +16,25 @@ function populateContainer(size) {
 
 populateContainer(16);
 
-function sizeChange(input) {
+function changeSize(input) {
   input >= 2 && input <= 100 ? populateContainer(input) : console.log("Error! Too many or too few squares");
 };
+
+function colorSquare() {
+  color === "random" ? this.style.backgroundColor = `hsl(${Math.random() *360}, 100%, 50%)`: this.style.backgroundColor = color;
+}
+
+function changeColor(choice) {
+  color = choice;
+}
+
+function resetContainer() {
+  let container = document.querySelector(".container");
+  let squares = container.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "#F3F8FF"));
+}
+
+
 
 
 
